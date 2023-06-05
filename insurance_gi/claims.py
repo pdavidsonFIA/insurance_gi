a28fe7ce1bd7e16758d57a056dc86c73201f7344
pdavidsonFIA <peterjd41@gmail.com>
2023-06-05 17:11:26 +0200

    adding claims runoff to use incurred and paid patterns

--- insurance_gi/claims.py
+++ insurance_gi/claims.py
@@ -5,46 +5,46 @@ This
 - Allows for build & release of IBNR & case reserves
 
 import pandas as pd
-pattern = [.9**(9-x) for x in range(10)] # cumulative
-pattern = [pattern[0]] + [j-i for i, j in zip(pattern[:-1], pattern[1:])] # incremental
-pattern = list(enumerate(pattern))
+paid = [.9**(9-x) for x in range(10)] # cumulative
+inc = [paid[0]+ (1-paid[0])/6 * x for x in range(6)] + (len(paid) - 6) * [1.] # linear over 6 months
+pattern = [(index, p, i) for index, (p, i) in enumerate(zip(paid, inc))]
+
+cum_to_inc = lambda patt: [patt[0]] + [j-i for i, j in zip(patt[:-1], patt[1:])] # incremental
+paid = list(enumerate(cum_to_inc(paid)))
+inc =  list(enumerate(cum_to_inc(inc)))
+pattern = [(index, p, i) for index, (p, i) in enumerate(zip(cum_to_inc(paid), cum_to_inc(inc)))]
+
 ref_date = pd.Period('2022-01')
-df = pd.DataFrame([[ref_date + i, -1, pattern] for i in range(5)], columns=['acc_month', 'ult', 'payment_pattern'])
+df = pd.DataFrame([[ref_date + i, -1, pattern] for i in range(5)], columns=['acc_month', 'ult', 'pattern'])
 
 """
 import pandas as pd
-import numpy as np
 
 
 def claims_runoff(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.explode('payment_pattern')
-    df[['rep_date_idx', 'claims_s']] = pd.DataFrame(df.payment_pattern.to_list(), index=df.index)
+    """
+    Payment patterns and ultimates
+    payment patterns should be (period, paid, incurred) -> cumulative
+    :param df:
+    :return:
+    """
+    df = df.explode('pattern')
+    df[['rep_date_idx', 'claim_s', 'incurred_s']] = pd.DataFrame(df.pattern.to_list(), index=df.index)
     df['rep_date'] = df.acc_month + df.rep_date_idx
 
-    df['d_claims'] = df.ult * df.claims_s
+    # Cumulative claims paid & reported
+    df['claims'] = df.ult * df.claim_s
+    df['incurred'] = df.ult * df.incurred_s
+    df['resv'] = df.incurred - df.claims
+    df['ibnr'] = df.ult - df.incurred
 
     # Rename blanks in the index
     df.index.names = [f"idx{i}" if v is None else v for i, v in enumerate(df.index.names)]
     grping_idx = df.index.names + ['acc_month']
 
-    # Arbitrary, zero initial reserves at end of first month
-    df['resv'] = 0.
-    # Cumulative totals calculate for ibnr & case:
-    df['claims'] = df.groupby(by=grping_idx)['d_claims'].cumsum()
-    df['incurred'] = df.resv + df.claims
-
-
-    df.loc[df.rep_date_idx > 0, 'incurred'] = np.nan
-    df.loc[df.rep_date_idx >= 6, 'incurred'] = df.loc[df.rep_date_idx >= 6, 'ult']
-    df.incurred = df.incurred.interpolate()
-
-    # IBNR should run to zero after 6 months. incurred and ibnr both negative, but ult a positive
-    df['ibnr'] = df.ult - df.incurred
+    # Deltas for claims & reserve positions
+    df['d_claims'] = df.groupby(by=grping_idx)['claims'].diff().fillna(df.claims)
     df['d_ibnr'] = df.groupby(by=grping_idx)['ibnr'].diff().fillna(df.ibnr)
-
-    # Recalculating case reserves:
-    df['resv'] = df['incurred'] - df['claims']
     df['d_resv'] = df.groupby(by=grping_idx)['resv'].diff().fillna(df.resv)
-
 
     return df
